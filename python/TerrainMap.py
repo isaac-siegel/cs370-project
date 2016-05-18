97e19646bc10c919af41c468d6df01942239a9dd
zkysar <zach.kysar@gmail.com>
2016-05-18 11:09:15 -0700

    Implemented TerrainMap

--- python/TerrainMap.py
+++ python/TerrainMap.py
@@ -1,1 +1,28 @@
-# constructor
+from Map import Map
+from Surroundings import Surroundings
+from TerrainTile import TerrainTile
+
+
+class TerrainMap(Map):
+    @staticmethod
+    def read_file(file_name):
+        # type: (string) -> Character[]
+        return [line.rstrip('\n') for line in open(file_name)]
+
+    @staticmethod
+    def flatten_array(array):
+        # type: (String[]) -> Character[]
+        return [char for line in array for char in line]
+
+    def get_surroundings(self, point):
+        # type: (Point) -> Surroundings
+        return Surroundings(*self.get_neighbors(point))
+
+    def __init__(self, file_name):
+        # type: (String) -> self
+        array = TerrainMap.read_file(file_name)
+        height = len(array)
+        width = len(array[0])
+        array = TerrainMap.flatten_array(array)
+        terrain_array = [TerrainTile(type) for type in array]
+        super(TerrainMap, self).__init__(terrain_array, width, height)
