90a03ece7f979f98393149e54ff5461442025bc9
Vincent Koeten <vrkoeten@cpp.edu>
2016-05-20 20:48:49 -0700

    Added Map.is_valid_move(state, movement

--- python/Map.py
+++ python/Map.py
@@ -53,8 +53,14 @@ class Map(object):
             to_string += "]\n"
         return to_string
 
-        def is_valid_state(self, state):
-            return Map.is_valid_point(state.point, self.width, self.height)
+    def is_valid_state(self, state):
+        return Map.is_valid_point(state.point, self.width, self.height)
+
+    def is_valid_move(self, state, movement):
+        move_state = State.copy(state)
+        move_state.move(movement)
+        return Map.is_valid_state(move_state)
+
 
     @staticmethod
     def point_to_index(point, width, height):
