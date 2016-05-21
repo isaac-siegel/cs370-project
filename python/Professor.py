4592b8dff72fb2ecdc8ff240c7e2129b11454eba
Isaac Siegel <siegelhorn@gmail.com>
2016-05-20 21:25:38 -0700

    fixed casting

--- python/Professor.py
+++ python/Professor.py
@@ -12,14 +12,14 @@ class Professor:
 
     def convert_neighbors_to_surroundings(self, neighbors, direction):
         surroundings = None
-        if direction == Direction.Directions.NORTH:
-            surroundings = Surroundings(neighbors.NORTH, neighbors.WEST, neighbors.SOUTH, neighbors.EAST)
-        elif direction == Direction.Directions.SOUTH:
-            surroundings = Surroundings(neighbors.SOUTH, neighbors.EAST, neighbors.NORTH, neighbors.WEST)
-        elif direction == Direction.Directions.EAST:
-            surroundings = Surroundings(neighbors.EAST, neighbors.NORTH, neighbors.WEST, neighbors.SOUTH)
-        elif direction == Direction.Directions.WEST:
-            surroundings = Surroundings(neighbors.WEST, neighbors.SOUTH, neighbors.EAST, neighbors.NORTH)
+        if direction == Direction(Direction.Directions.NORTH):
+            surroundings = Surroundings(neighbors['NORTH'], neighbors['WEST'], neighbors['SOUTH'], neighbors['EAST'])
+        elif direction == Direction(Direction.Directions.SOUTH):
+            surroundings = Surroundings(neighbors['SOUTH'], neighbors['EAST'], neighbors['NORTH'], neighbors['WEST'])
+        elif direction == Direction(Direction.Directions.EAST):
+            surroundings = Surroundings(neighbors['EAST'], neighbors['NORTH'], neighbors['WEST'], neighbors['SOUTH'])
+        elif direction == Direction(Direction.Directions.WEST):
+            surroundings = Surroundings(neighbors['WEST'], neighbors['SOUTH'], neighbors['EAST'], neighbors['NORTH'])
 
         print(surroundings)
         return surroundings
@@ -29,7 +29,7 @@ class Professor:
         neighbors = self.terrain_map.get_neighbors(self.state.point)
         prof_direction = self.state.direction
 
-        return convert_neighbors_to_surroundings(neighbors, prof_direction)
+        return self.convert_neighbors_to_surroundings(neighbors, prof_direction)
 
 
     def __str__(self):
