b7adf42cc22596a1beac68efc3b3970073971334
Isaac Siegel <siegelhorn@gmail.com>
2016-05-25 21:08:21 -0700

    implemented direction_to_move

--- python/State.py
+++ python/State.py
@@ -19,6 +19,7 @@ class State(object):
 
     @staticmethod
     def move_to_direction(facing_direction, movement):
+        # untested
         facing_north_go_forward =  facing_direction == Directions.NORTH and movement == Moves.FORWARD
         facing_north_go_backward = facing_direction == Directions.NORTH and movement == Moves.BACKWARD
         facing_north_go_left =     facing_direction == Directions.NORTH and movement == Moves.LEFT
@@ -53,10 +54,41 @@ class State(object):
             # Go WEST
             return Directions.WEST
 
-
     @staticmethod
     def direction_to_move(facing_direction, move_direction):
-        raise NotImplemented
+        # untested
+        facing_north_go_north =  facing_direction == Directions.NORTH and move_direction == Directions.NORTH
+        facing_north_go_south =  facing_direction == Directions.NORTH and move_direction == Directions.SOUTH
+        facing_north_go_east  =  facing_direction == Directions.NORTH and move_direction == Directions.EAST
+        facing_north_go_west  =  facing_direction == Directions.NORTH and move_direction == Directions.WEST
+
+        facing_south_go_north =  facing_direction == Directions.SOUTH and move_direction == Directions.NORTH
+        facing_south_go_south =  facing_direction == Directions.SOUTH and move_direction == Directions.SOUTH
+        facing_south_go_east  =  facing_direction == Directions.SOUTH and move_direction == Directions.EAST
+        facing_south_go_west  =  facing_direction == Directions.SOUTH and move_direction == Directions.WEST
+
+        facing_east_go_north  =  facing_direction == Directions.EAST  and move_direction == Directions.NORTH
+        facing_east_go_south  =  facing_direction == Directions.EAST  and move_direction == Directions.SOUTH
+        facing_east_go_east   =  facing_direction == Directions.EAST  and move_direction == Directions.EAST
+        facing_east_go_west   =  facing_direction == Directions.EAST  and move_direction == Directions.WEST
+
+        facing_west_go_north  =  facing_direction == Directions.WEST  and move_direction == Directions.NORTH
+        facing_west_go_south  =  facing_direction == Directions.WEST  and move_direction == Directions.SOUTH
+        facing_west_go_east   =  facing_direction == Directions.WEST  and move_direction == Directions.EAST
+        facing_west_go_west   =  facing_direction == Directions.WEST  and move_direction == Directions.WEST
+
+        if facing_north_go_north or facing_east_go_east or facing_south_go_south or facing_west_go_west:
+            # Go FORWARD
+            return Moves.FORWARD
+        elif facing_north_go_south or facing_east_go_west or facing_south_go_north or facing_west_go_east:
+            # Go BACKWARD
+            return Moves.BACKWARD
+        elif facing_north_go_west or facing_east_go_north or facing_south_go_east or facing_west_go_south:
+            # Go LEFT
+            return Moves.LEFT
+        elif facing_north_go_east  or facing_east_go_south or facing_south_go_west or facing_west_go_north:
+            # Go RIGHT
+            return Moves.RIGHT
 
 
     def move(self, movement):
