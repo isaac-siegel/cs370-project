c4699769649f1de73dd3d5f8fa4de8e34f9d452f
Isaac Siegel <siegelhorn@gmail.com>
2016-05-30 20:26:24 -0700

    Added Display_Slideshow functionality for steps pngs

---
+++ python/Display_Slideshow.py
@@ -0,0 +1,20 @@
+import pylab as pl
+import os
+
+def display():
+    os.chdir("steps")
+    
+    files = [f for f in os.listdir('.') if f[-3:] == 'png']
+
+    img = None
+    for f in files:
+        im=pl.imread(f)
+        if img is None:
+            img = pl.imshow(im)
+        else:
+            img.set_data(im)
+        pl.pause(1)
+        pl.draw()
+
+display()
+
